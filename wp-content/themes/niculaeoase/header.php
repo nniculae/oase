ae363b5b29500b2bafe99522126dfccf59123b9c
Niculae Niculae <niculaenick@gmail.com>
2018-04-16 13:14:14 +0200

    Contact form and header styled

--- wp-content/themes/niculaeoase/header.php
+++ wp-content/themes/niculaeoase/header.php
@@ -7,75 +7,93 @@
  * @package understrap
  */
 
-$container = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod('understrap_container_type');
 ?>
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="mobile-web-app-capable" content="yes">
-	<meta name="apple-mobile-web-app-capable" content="yes">
-	<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<?php wp_head(); ?>
-</head>
+	<!DOCTYPE html>
+	<html <?php language_attributes(); ?>>
 
-<body <?php body_class(); ?>>
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-title" content="<?php bloginfo('name'); ?> - <?php bloginfo('description'); ?>">
+		<link rel="profile" href="http://gmpg.org/xfn/11">
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+		<?php wp_head(); ?>
+	</head>
 
-<div class="hfeed site" id="page">
+	<body <?php body_class(); ?>>
 
-	<!-- ******************* The Navbar Area ******************* -->
-	<div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
+		<div class="hfeed site" id="page">
 
-		<a class="skip-link screen-reader-text sr-only" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
+			<!-- ******************* The Navbar Area ******************* -->
+			<div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
 
-		<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+				<a class="skip-link screen-reader-text sr-only" href="#content">
+					<?php esc_html_e('Skip to content', 'understrap'); ?>
+				</a>
 
-		<?php if ( 'container' == $container ) : ?>
-			<div class="container" >
-		<?php endif; ?>
+				<nav class="navbar navbar-expand-md navbar-light bg-light">
 
-					<!-- Your site title as branding in the menu -->
-					<?php if ( ! has_custom_logo() ) { ?>
+					<?php if ('container' == $container) : ?>
+					<div class="container">
+						<?php endif; ?>
 
-						<?php if ( is_front_page() && is_home() ) : ?>
+						<!-- Your site title as branding in the menu -->
+						<?php if (!has_custom_logo()) {
+    ?>
 
-							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
-							
+						<?php if (is_front_page() && is_home()) : ?>
+
+						<h1 class="navbar-brand mb-0">
+							<a rel="home" href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>"
+							    itemprop="url">
+								<?php bloginfo('name'); ?>
+							</a>
+						</h1>
+
 						<?php else : ?>
 
-							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
-						
+						<a class="navbar-brand" rel="home" href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>"
+						    itemprop="url">
+							<?php bloginfo('name'); ?>
+						</a>
+
 						<?php endif; ?>
-						
-					
-					<?php } else {
-						the_custom_logo();
-					} ?><!-- end custom logo -->
 
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
 
-				<!-- The WordPress Menu goes here -->
-				<?php wp_nav_menu(
-					array(
-						'theme_location'  => 'primary',
-						'container_class' => 'collapse navbar-collapse',
-						'container_id'    => 'navbarNavDropdown',
-						'menu_class'      => 'navbar-nav',
-						'fallback_cb'     => '',
-						'menu_id'         => 'main-menu',
-						'walker'          => new understrap_WP_Bootstrap_Navwalker(),
-					)
-				); ?>
-			<?php if ( 'container' == $container ) : ?>
-			</div><!-- .container -->
-			<?php endif; ?>
+						<?php
+} else {
+        the_custom_logo();
+    } ?>
+							<!-- end custom logo -->
 
-		</nav><!-- .site-navigation -->
+							<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown"
+							    aria-expanded="false" aria-label="Toggle navigation">
+								<span class="navbar-toggler-icon"></span>
+							</button>
 
-	</div><!-- #wrapper-navbar end -->
+							<!-- The WordPress Menu goes here -->
+							<?php wp_nav_menu(
+                    [
+                        'theme_location'  => 'primary',
+                        'container_class' => 'collapse navbar-collapse',
+                        'container_id'    => 'navbarNavDropdown',
+                        'menu_class'      => 'navbar-nav',
+                        'fallback_cb'     => '',
+                        'menu_id'         => 'main-menu',
+                        'walker'          => new understrap_WP_Bootstrap_Navwalker(),
+                    ]
+                ); ?>
+							<?php if ('container' == $container) : ?>
+					</div>
+					<!-- .container -->
+					<?php endif; ?>
+
+				</nav>
+				<!-- .site-navigation -->
+
+			</div>
+			<!-- #wrapper-navbar end -->
