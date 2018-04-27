db2bac1f0b32c259a6b81bcfbd64271aba577720
Niculae Niculae <niculaenick@gmail.com>
2018-04-27 17:34:10 +0200

    front page icons added

--- wp-content/themes/niculaeoase/front-page.php
+++ wp-content/themes/niculaeoase/front-page.php
@@ -26,7 +26,9 @@ $container   = get_theme_mod('understrap_container_type');
 
 				<main id="home-block-container">
 					<div class="hometop blocks">
+						
 						<a href="/info" class="about homeblock expanda">
+						<span class="fa-icons"><i class="fa fa-pagelines"></i></span>
 							<h1>Over
 								<br>
 								<strong>Ons</strong>
@@ -36,12 +38,15 @@ $container   = get_theme_mod('understrap_container_type');
 						<div class="hometopright">
 							<!-- 2 -->
 							<a href="/activiteiten" class="activiteiten homeblock expanda">
+							<span class="fa-icons"><i class="fa fa-meetup"></i></span>
 								<h1>Onze
 									<br>
 									<strong> Activiteiten</strong>
 								</h1>
 							</a>
 							<a href="/nieuws" class="nieuws homeblock expanda">
+							<span class="fa-icons"><i class="fa fa-newspaper-o"></i></span>
+							
 								<h1>Laatste
 									<br>
 									<strong> artikels</strong>
@@ -54,12 +59,14 @@ $container   = get_theme_mod('understrap_container_type');
 							<div class="homebottomleft">
 
 								<a href="/fotos" class="fotos homeblock expanda">
+								<span class="fa-icons"><i class="fa fa-camera"></i></span>
 									<h1>Onze
 										<br>
 										<strong>Fotos</strong>
 									</h1>
 								</a>
 								<a href="/sponsors" class="sponsors homeblock expanda">
+								<span class="fa-icons"><i class="fa fa-bullhorn"></i></span>
 									<h1>Onze
 										<br>
 										<strong>Sponsors</strong>
@@ -69,6 +76,7 @@ $container   = get_theme_mod('understrap_container_type');
 							</div>
 							
 							<a href="/info" class="contact homeblock expanda">
+							<span class="fa-icons"><i class="fa fa-envelope-square"></i></span>
 								<h1>Contact
 									<br><strong>Ons</strong>
 								</h1>
