8c7096ad77bdd50215cd77cf769a0c38a7e90596
Niculae Niculae <niculaenick@gmail.com>
2018-04-27 15:11:05 +0200

    Add NL translation in the content-languages dir

--- wp-content/themes/niculaeoase/inc/setup.php
+++ wp-content/themes/niculaeoase/inc/setup.php
@@ -29,7 +29,8 @@ if ( ! function_exists ( 'understrap_setup' ) ) {
 		 * to change 'understrap' to the name of your theme in all the template files
 		 */
 		load_theme_textdomain( 'understrap', get_template_directory() . '/languages' );
-
+		
+		
 		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
 
